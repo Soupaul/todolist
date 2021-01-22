29f91d93a42a6c853c8828212a9a29b67c11e5cb
Nirvik <nirvik.agarwal@gmail.com>
2021-01-22 19:13:59 +0530

    complete project

--- server.js
+++ server.js
@@ -7,16 +7,41 @@ app.use(express.static("public"));
 
 let port = 3000;
 
-const users = [];
+let todos = [];
 
 const weekdays = ["Monday", "tuesday", "Friday"];
 
 app.set("view engine", "ejs");
 
+function getday() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  return today.toLocaleDateString("en-US", options);
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.render("home", { name: "Mohit", dept: "CSE" });
+  const day = getday();
+  res.render("list", { tasks: todos, weekday: day });
+});
+
+app.post("/", function (req, res) {
+  const task = req.body.newTask;
+  todos.push(task);
+  res.redirect("/");
+});
+
+app.post("/delete", function (req, res) {
+  const position = req.body.position;
+  todos = todos.filter(function (todo, index) {
+    return index != position;
+  });
+  res.redirect("/");
 });
 
 app.post("/form", function (req, res) {
